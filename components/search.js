ca8c8fe874ac6514b2a5da8dc614b5df854719e6
Aleksandra Usacheva <lazercore@me.com>
2022-11-20 02:43:30 +0100

    markup fixes

--- components/search.js
+++ components/search.js
@@ -1,4 +1,6 @@
 import formStyles from '../styles/forms.module.scss'
+import styles from '../styles/Search.module.scss'
+
 import cx from "classnames";
 import { useRouter } from 'next/router'
 
@@ -6,7 +8,7 @@ const Search = () => {
   const router = useRouter()
 
   return (
-    <form className={cx(formStyles.form, formStyles.horizontal)} onSubmit={e => {
+    <form className={cx(formStyles.form, formStyles.horizontal, styles.container)} onSubmit={e => {
       e.preventDefault()
     }}>
       <input placeholder='Search' type="text" name='search' required onChange={e => {
